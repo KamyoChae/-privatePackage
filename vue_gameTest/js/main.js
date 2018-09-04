6ca9e80bdb99b12e27ed4fc7c8de74d25761c95f
Y_林泉 <32585968+KamyoChae@users.noreply.github.com>
2018-09-05 00:04:05 +0800

    Update main.js

--- vue_gameTest/js/main.js
+++ vue_gameTest/js/main.js
@@ -15,6 +15,7 @@ new Vue({
         overGame: function () {
             alert("游戏结束")
             this.start = !this.start
+            this.logs = []
         },
         nomarlAttact: function () {
             this.skill(20,3)
@@ -26,7 +27,9 @@ new Vue({
             if(this.wuyao<=90){
 
                 this.wuyao += 10;
-                this.logs.unshift({green:10})
+                this.logs.unshift({
+                    myclass: "green",
+                    tips:"使用治疗，生命+"+10,})
             }
             this.skill(20,3)
         },
@@ -41,9 +44,15 @@ new Vue({
                     return
                 }
                 this.boss -= this.attact(s1, s2);
-                this.logs.unshift({blue:this.attact(s1, s2)})
+                this.logs.unshift({
+                    myclass:"blue",
+                    tips:"攻击boss,boss血量减少" + this.attact(s1, s2),
+                })
                 this.wuyao -= this.attact(15, 3);
-                this.logs.unshift({red:this.attact(15, 3)})
+                this.logs.unshift({
+                    myclass: "red",
+                    tips:"被boss攻击，血量减少"+this.attact(15, 3)
+                })
             }
         },
         attact: function (max, min) {
