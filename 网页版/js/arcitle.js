db7d99d186a095c2db44bd00ad82f831d62256c6
kamyochae <1465678596@qq.com>
2018-09-12 09:20:03 +0800

    一些优化

---
+++ 网页版/js/arcitle.js
@@ -0,0 +1,16 @@
+function $(ele){
+ var doc = document.querySelector(ele)
+ if(doc){
+     return doc
+ }else{
+     return new Error("dom节点有误")
+ }
+}
+
+var paybtn = $('.pay-btn')
+var article = $('.article')
+var recover = $('.recover')
+paybtn.addEventListener("click",function(){
+    article.className = "article"
+    recover.style.display = 'none'
+},false)
